6f02041f85981694a61412eb885ef78e9a93d8e1
Winnie <winniebosy@gmail.com>
2019-03-10 02:09:04 +0300

    modified the hours const section to ensure that the clock correctly displays hours

--- main.js
+++ main.js
@@ -4,11 +4,26 @@
 
 function setDate(){
    const now = new Date();
+
+   /*gets the clocks seconds hand */
    const seconds = now.getSeconds();
    const secondsDegrees = ((seconds / 60) * 360) + 90;
    const secondHand = document.querySelector('.second-hand');
    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-   console.log(seconds);
+    
+
+   /*gets the clocks minute hand */
+   const minutes =now.getMinutes();
+   const minutesDegrees = ((minutes / 60) * 360) + 90;
+   const minutesHand = document.querySelector('.min-hand');
+   minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  /*gets the clocks hour hand */
+   const hour = now.getMinutes();
+   const hourDegrees = ((minutes/ 12) * 360) + 90;
+   const hourHand = document.querySelector('.hour-hand');
+   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+   
 }
 /*facilitates in ensuirng that the clock is running every second*/
   setInterval(setDate, 1000);
